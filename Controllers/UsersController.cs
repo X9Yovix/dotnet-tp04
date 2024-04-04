1b7dc71709a3b75104bf2332d83b6fd9e0b7edfd
Karim Ouazzou <karimouazzou@gmail.com>
2024-04-04 19:45:26 +0100

    Création du contrôleur et des vues

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TP04.Models;
+
+namespace TP04.Controllers
+{
+    public class UsersController : Controller
+    {
+        private UsersEntities db = new UsersEntities();
+
+        // GET: Users
+        public ActionResult Index()
+        {
+            return View(db.USERS.ToList());
+        }
+
+        // GET: Users/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            USERS uSERS = db.USERS.Find(id);
+            if (uSERS == null)
+            {
+                return HttpNotFound();
+            }
+            return View(uSERS);
+        }
+
+        // GET: Users/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Users/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Age,Job")] USERS uSERS)
+        {
+            if (ModelState.IsValid)
+            {
+                db.USERS.Add(uSERS);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(uSERS);
+        }
+
+        // GET: Users/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            USERS uSERS = db.USERS.Find(id);
+            if (uSERS == null)
+            {
+                return HttpNotFound();
+            }
+            return View(uSERS);
+        }
+
+        // POST: Users/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Age,Job")] USERS uSERS)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(uSERS).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(uSERS);
+        }
+
+        // GET: Users/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            USERS uSERS = db.USERS.Find(id);
+            if (uSERS == null)
+            {
+                return HttpNotFound();
+            }
+            return View(uSERS);
+        }
+
+        // POST: Users/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            USERS uSERS = db.USERS.Find(id);
+            db.USERS.Remove(uSERS);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
